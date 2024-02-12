e042db6f524886ad51511d9680e1005013ea6bae
Prasad Gawade <prasadgawade249@gmail.com>
2024-02-12 15:58:18 +0530

    project discription added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Project Name - Mediadent</h1>
+      <h6>functionality -</h6>
+      <p>Classifieds Section: Allow students to buy, sell, or exchange textbooks, study materials, and other educational resources within their community.
+
+        Event Calendar: Include a calendar feature where students can create and discover academic events, such as study groups, workshops, seminars, and campus activities.
+
+        Polls and Surveys: Enable students to create polls and surveys to gather opinions, feedback, and preferences on various academic topics, courses, or campus issues.
+
+        Resource Sharing: Implement a feature for students to share helpful resources such as study guides, lecture notes, past exam papers, and reference materials within their community.
+
+        Study Buddy Finder: Develop a feature where students can search for study partners or group members based on shared interests, courses, or study preferences.Virtual Study Rooms: Provide virtual study rooms where students can join online study sessions, collaborate on group projects, and engage in focused study sessions with peers.
+
+        Project Collaboration: Facilitate collaboration on academic projects by allowing students to create project groups, share files, collaborate on documents, and track project progress.
+
+        Safety and Wellness Resources: Offer resources and support for students' well-being, including mental health resources, campus safety tips, and emergency contact information.
+
+        Peer Review and Feedback: Allow students to submit assignments, essays, or projects for peer review and feedback within their community. Include features for constructive criticism and improvement suggestions.
+
+        Campus News and Announcements: Share important campus news, announcements, and updates relevant to students, such as academic deadlines, campus events, and policy changes.</p>
     </div>
   );
 }
